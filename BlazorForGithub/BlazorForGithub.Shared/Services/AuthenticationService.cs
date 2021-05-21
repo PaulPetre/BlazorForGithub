ba2428c556c4db10ba0bdba791a944778ec2298b
Paul Petre <paul.petre98@yahoo.com>
2021-05-21 18:13:31 +0300

    project added

---
+++ BlazorForGithub/BlazorForGithub.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using AKSoftware.WebApi.Client;
+using BlazorForGithub.Shared.Models;
+
+namespace BlazorForGithub.Shared.Services
+{
+    public class AuthenticationService
+    {
+        private readonly string _baseUrl;
+
+        private ServiceClient client = new();
+
+        public AuthenticationService(string url)
+        {
+            _baseUrl = url;
+        }
+
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/register", request);
+
+            return response.Result;
+        }
+
+        public async Task<UserManagerResponse> LoginUserAsync(LoginRequest request)
+        {
+            var response = await client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/login", request);
+
+            return response.Result;
+        }
+    }
+}
---
+++ BlazorForGithub/BlazorForGithub/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using BlazorForGithub.Shared.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorForGithub
+{
+    public class Program
+    {
+        private const string URL = "https://plannerappserver20200228091432.azurewebsites.net";
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped<AuthenticationService>(it =>
+            {
+                return new AuthenticationService(URL);
+            });
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, LocalAuthenticationStateProvider>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
