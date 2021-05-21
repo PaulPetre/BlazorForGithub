e91ebfaedaf2c145d143c9b6f5a2d08dbf31d134
Paul Petre <paul.petre98@yahoo.com>
2021-05-21 18:27:57 +0300

    qd

--- BlazorForGithub/BlazorForGithub/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ BlazorForGithub/BlazorForGithub/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,119 +13,119 @@ namespace BlazorForGithub.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 1 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 2 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 3 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 4 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 5 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 6 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 7 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 8 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 9 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using BlazorForGithub;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 10 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using BlazorForGithub.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 11 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using BlazorForGithub.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 12 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 13 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 15 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Blazored.LocalStorage;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 16 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
+#line 17 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Paul\BlazorForGithub\BlazorForGithub\BlazorForGithub\Pages\Index.razor"
+#line 4 "C:\git\BlazorForGithub\BlazorForGithub\BlazorForGithub\Pages\Index.razor"
            [Authorize]
 
 #line default
